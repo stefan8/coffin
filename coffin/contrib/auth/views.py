73a9110f6619caf12324bbc194b6a04c83613ec3
Grzegorz Szczepanczyk <g.szczepanczyk@hiddendata.co>
2012-03-13 13:13:37 +0100

    fixed auth views

--- coffin/contrib/auth/views.py
+++ coffin/contrib/auth/views.py
@@ -1,6 +1,6 @@
 import inspect
-
 from django.contrib.auth.views import *
+from django.contrib.sites.models import Site, RequestSite
 
 # XXX: maybe approach this as importing the entire model, and doing string replacements
 # on the template and shortcut import lines?
@@ -8,14 +8,37 @@ from django.contrib.auth.views import *
 from coffin.shortcuts import render_to_response
 from coffin.template import RequestContext, loader
 
-exec inspect.getsource(logout)
-exec inspect.getsource(password_change_done)
-exec inspect.getsource(password_reset)
-exec inspect.getsource(password_reset_confirm)
-exec inspect.getsource(password_reset_done)
-exec inspect.getsource(password_reset_complete)
+import inspect
+import re
+def getsource(func, func_name=None, regex=None):
+    if not func_name:
+        func_name = func.__name__
 
-exec inspect.getsource(password_change.view_func)
+    if not regex:
+        regex = re.compile('^def %s' % func_name, re.MULTILINE)
+
+    try:
+        source = inspect.getsource(func)
+        if regex.search(source):
+            return source
+    except TypeError:
+        pass
+
+    func_closure = getattr(func, 'func_closure', []) or []
+    for cell in func_closure:
+        source = getsource(cell.cell_contents, func_name, regex)
+        if source:
+            return source
+    return None
+
+exec getsource(logout)
+exec getsource(password_change_done)
+exec getsource(password_reset)
+exec getsource(password_reset_confirm)
+exec getsource(password_reset_done)
+exec getsource(password_reset_complete)
+
+exec getsource(password_change)
 password_change = login_required(password_change)
 
 # XXX: this function uses a decorator, which calls functools.wraps, which compiles the code
@@ -46,5 +69,5 @@ def login(request, template_name='registration/login.html', redirect_field_name=
         redirect_field_name: redirect_to,
         'site': current_site,
         'site_name': current_site.name,
-    }, context_instance=RequestContext(request))
+        }, context_instance=RequestContext(request))
 login = never_cache(login)
