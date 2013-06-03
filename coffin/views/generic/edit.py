c461354d293bf28b50634b081af5b9be9c18ecde
Rafał Jagoda <rafaljagoda@gmail.com>
2013-06-03 15:13:28 +0200

    TemplateResponseMixin added

--- coffin/views/generic/edit.py
+++ coffin/views/generic/edit.py
@@ -1,8 +1,13 @@
-from django.views.generic.edit import CreateView as _CreateView, UpdateView as _UpdateView, DeleteView as _DeleteView, FormView as _FormView
+from django.views.generic.base import TemplateResponseMixin as _TemplateResponseMixin
+from django.views.generic.edit import CreateView as _CreateView, \
+    UpdateView as _UpdateView, \
+    DeleteView as _DeleteView, \
+    FormView as _FormView
+
 from coffin.views.decorators import template_response
 
-__all__ = ['CreateView', 'UpdateView', 'DeleteView', 'FormView']
+__all__ = ['CreateView', 'UpdateView', 'DeleteView', 'FormView', 'TemplateResponseMixin']
 
-CreateView, UpdateView, DeleteView, FormView = map(template_response,
-        (_CreateView, _UpdateView, _DeleteView, _FormView)) 
+CreateView, UpdateView, DeleteView, FormView, TemplateResponseMixin = map(template_response,
+        (_CreateView, _UpdateView, _DeleteView, _FormView, _TemplateResponseMixin))
 
